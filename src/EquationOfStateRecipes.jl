f5082fdddf66ab6986b3718b61b39161baf37242
singularitti <singularitti@outlook.com>
2020-12-16 05:02:33 -0500

    Add other `EquationOfStateOfSolids` support

--- src/EquationOfStateRecipes.jl
+++ src/EquationOfStateRecipes.jl
@@ -1,14 +1,23 @@
 module EquationOfStateRecipes
 
-using EquationsOfStateOfSolids.Collections: EnergyEOS, PressureEOS, BulkModulusEOS
-using RecipesBase
+using EquationsOfStateOfSolids.Collections:
+    EquationOfStateOfSolids, EnergyEOS, PressureEOS, BulkModulusEOS
+using RecipesBase: @recipe
 using Unitful: ustrip
 
-@recipe function f(eos::EnergyEOS, volumes::AbstractVector = (0.5:0.01:1.1) * eos.param.v0)
-    xlabel --> "volume"
-    yguide --> "energy"
-    markershape --> :diamond
+import RecipesBase
+
+@recipe function f(
+    eos::EquationOfStateOfSolids,
+    volumes::AbstractVector = (0.5:0.01:1.1) * eos.param.v0,
+)
+    xguide --> "volume"
+    yguide --> _yprefix(typeof(eos))
     return map(ustrip, volumes), map(ustrip ∘ eos, volumes)
 end
+
+_yprefix(::Type{<:EnergyEOS}) = "energy"
+_yprefix(::Type{<:PressureEOS}) = "pressure"
+_yprefix(::Type{<:BulkModulusEOS}) = "bulk modulus"
 
 end
