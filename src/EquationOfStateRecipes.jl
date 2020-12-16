dbe82ee191da91b8da4060ad7b408b22e66b1547
singularitti <singularitti@outlook.com>
2020-12-16 04:53:17 -0500

    Get the first plot

--- src/EquationOfStateRecipes.jl
+++ src/EquationOfStateRecipes.jl
@@ -1,5 +1,14 @@
 module EquationOfStateRecipes
 
-# Write your package code here.
+using EquationsOfStateOfSolids.Collections: EnergyEOS, PressureEOS, BulkModulusEOS
+using RecipesBase
+using Unitful: ustrip
+
+@recipe function f(eos::EnergyEOS, volumes::AbstractVector = (0.5:0.01:1.1) * eos.param.v0)
+    xlabel --> "volume"
+    yguide --> "energy"
+    markershape --> :diamond
+    return map(ustrip, volumes), map(ustrip ∘ eos, volumes)
+end
 
 end
