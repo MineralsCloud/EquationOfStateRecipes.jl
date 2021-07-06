3bc03e35e429f6486ef7ef8fb8b5d44f6bdcd088
Documenter.jl <documenter@juliadocs.github.io>
2021-07-06 04:12:18 +0000

    build based on 1c031b4

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EquationOfStateRecipes","category":"page"},{"location":"#EquationOfStateRecipes","page":"Home","title":"EquationOfStateRecipes","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EquationOfStateRecipes]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EquationOfStateRecipes","category":"page"},{"location":"#EquationOfStateRecipes","page":"Home","title":"EquationOfStateRecipes","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EquationOfStateRecipes.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EquationOfStateRecipes]","category":"page"}]
 }
