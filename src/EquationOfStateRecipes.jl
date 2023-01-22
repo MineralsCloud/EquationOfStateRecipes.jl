11a70be05669ca9474f8b317b613cc91f2d4559f
singularitti <singularitti@outlook.com>
2023-01-22 01:56:49 -0500

    Fix signatures of `_yguideprefix` & `_ydefaultunit`

--- src/EquationOfStateRecipes.jl
+++ src/EquationOfStateRecipes.jl
@@ -12,25 +12,25 @@ import RecipesBase
     volumes::AbstractVector = (0.5:0.01:1.1) * eos.param.v0,
 )
     if eltype(volumes) <: AbstractQuantity
-        u = _yunit(typeof(eos))
-        xguide --> "volume (angstrom^3)"
-        yguide --> _yprefix(typeof(eos)) * " ($u)"
+        u = _ydefaultunit(typeof(eos))
+        xguide --> "volume"
+        yguide --> _yguideprefix(eos) * " ($u)"
         x = map(Base.Fix1(ustrip, u"angstrom^3"), volumes)
         y = map(Base.Fix1(ustrip, u) ∘ eos, volumes)
         return x, y
     else
         xguide --> "volume"
-        yguide --> _yguideprefix(typeof(eos))
+        yguide --> _yguideprefix(eos)
         return volumes, map(eos, volumes)
     end
 end
 
-_yguideprefix(::Type{<:EnergyEquation}) = "energy"
-_yguideprefix(::Type{<:PressureEquation}) = "pressure"
-_yguideprefix(::Type{<:BulkModulusEquation}) = "bulk modulus"
+_yguideprefix(::EnergyEquation) = "energy"
+_yguideprefix(::PressureEquation) = "pressure"
+_yguideprefix(::BulkModulusEquation) = "bulk modulus"
 
-_ydefaultunit(::Type{<:EnergyEquation}) = u"eV"
-_ydefaultunit(::Type{<:PressureEquation}) = u"GPa"
-_ydefaultunit(::Type{<:BulkModulusEquation}) = u"GPa"
+_ydefaultunit(::EnergyEquation) = u"eV"
+_ydefaultunit(::PressureEquation) = u"GPa"
+_ydefaultunit(::BulkModulusEquation) = u"GPa"
 
 end
