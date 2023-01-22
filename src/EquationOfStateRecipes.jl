2baa74c65a1c8012bd90d6a5268bc9022aac5247
singularitti <singularitti@outlook.com>
2023-01-22 02:30:58 -0500

    Rewrite `plot` for `EquationOfStateOfSolids`

--- src/EquationOfStateRecipes.jl
+++ src/EquationOfStateRecipes.jl
@@ -2,27 +2,27 @@ module EquationOfStateRecipes
 
 using EquationsOfStateOfSolids:
     EquationOfStateOfSolids, EnergyEquation, PressureEquation, BulkModulusEquation
-using RecipesBase: @recipe
+using RecipesBase: @recipe, @series
 using Unitful: AbstractQuantity, ustrip, @u_str
 
 import RecipesBase
 
 @recipe function f(
     eos::EquationOfStateOfSolids,
-    volumes::AbstractVector = (0.5:0.01:1.1) * eos.param.v0,
+    volumes=eos.param.v0 .* (0.5:0.01:1.1)
 )
-    if eltype(volumes) <: AbstractQuantity
-        u = _ydefaultunit(typeof(eos))
-        xguide --> "volume"
-        yguide --> _yguideprefix(eos) * " ($u)"
-        x = map(Base.Fix1(ustrip, u"angstrom^3"), volumes)
-        y = map(Base.Fix1(ustrip, u) ∘ eos, volumes)
-        return x, y
-    else
-        xguide --> "volume"
-        yguide --> _yguideprefix(eos)
-        return volumes, map(eos, volumes)
+    xguide --> "volume"
+    yguide --> _yguideprefix(eos)
+    palette --> :tab20
+    frame --> :box
+    xlims --> extrema(volumes)
+    @series begin
+        seriestype --> :scatter
+        label := ""
+        volumes, map(eos, volumes)
     end
+    seriestype --> :path
+    return volumes, map(eos, volumes)
 end
 
 _yguideprefix(::EnergyEquation) = "energy"
