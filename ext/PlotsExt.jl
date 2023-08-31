08866191417ac0ea5a993eaa3354ee2b98b7cc68
singularitti <singularitti@outlook.com>
2023-08-31 02:07:21 -0400

    Rename variables in ext/PlotsExt.jl

--- ext/PlotsExt.jl
+++ ext/PlotsExt.jl
@@ -2,23 +2,23 @@ module PlotsExt
 
 using RecipesBase: AbstractPlot, plot, plot!
 using EquationsOfStateOfSolids: EquationOfStateOfSolids, PressureEquation
-using Plots: xticks, twiny, yticks
+using Plots: current, xticks, twiny, yticks
 
 import EquationOfStateRecipes: pressurescaleplot, pressurescaleplot!
 
 function pressurescaleplot!(
-    p::AbstractPlot, eos::EquationOfStateOfSolids, volumes=eos.param.v0 .* (0.5:0.01:1.1)
+    plt::AbstractPlot, eos::EquationOfStateOfSolids, volumes=eos.param.v0 .* (0.5:0.01:1.1)
 )
     pressures = map(PressureEquation(eos), volumes)
     plot!(eos, volumes)
-    p1, p2 = extrema(pressures)
-    axis2 = range(p1, p2; length=length(first(first(xticks(p)))))
-    plot!(twiny(p), [], []; label="", xlims=(p1, p2), xticks=axis2, xaxis="pressure")
-    plot!(p; framestyle=:box)
-    return p
+    pₘᵢₙ, pₘₐₓ = extrema(pressures)
+    xaxis₂ = range(pₘᵢₙ, pₘₐₓ; length=length(first(first(xticks(plt)))))
+    plot!(twiny(plt), [], []; label="", xlims=(pₘᵢₙ, pₘₐₓ), xticks=xaxis₂, xaxis="pressure")
+    plot!(plt; framestyle=:box)
+    return plt
 end
 pressurescaleplot!(eos::EquationOfStateOfSolids, volumes=eos.param.v0 .* (0.5:0.01:1.1)) =
-    pressurescaleplot!(plot(), eos, volumes)
+    pressurescaleplot!(current(), eos, volumes)
 pressurescaleplot(eos::EquationOfStateOfSolids, volumes=eos.param.v0 .* (0.5:0.01:1.1)) =
     pressurescaleplot!(plot(), eos, volumes)
 
